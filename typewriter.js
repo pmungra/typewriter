3321c5ce9a34dd4d8a6cab556b32f52b2ba058bc
Your Name <parth.mungra94@gmail.com>
2021-12-02 12:34:21 +0000

    Adding process.stdout.write

--- typewriter.js
+++ typewriter.js
@@ -1,5 +1,15 @@
 const sentence = "hello there from lighthouse labs";
+
+// add each letter of string in new line in no time
+
+/*
 for (const char of sentence) {
   console.log(char);
 }
+*/
 
+//Adding process.stdout.write
+
+for (const char of sentence) {
+  process.stdout.write(char);
+}
